3b4d2bcac9fc1d9fc08c0c862b77457ce4dfabe3
stagefrightband <frepat@student.sisd.net>
2026-09-23 10:22:17 -0600

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    inlineCss: true,
-  },
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
 };
